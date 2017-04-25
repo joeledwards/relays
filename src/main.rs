cafa69541529e0d89593282d60eaf31f496ae87b
Joel Edwards <joeledwards@gmail.com>
2017-04-25 05:02:05 +0000

    Started working on the controller for the 8-relay board.

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+extern crate sysfs_gpio;
+
+use sysfs_gpio::{Direction, Pin};
+use std::thread::sleep;
+use std::time::Duration;
+use std::collections::HashMap;
+
+fn main() {
+  let mut relays = HashMap::new();
+  mut.insert(1, 4); // 7 - 4
+  mut.insert(2, 17); // 11 - 17
+  mut.insert(3, 27); // 13 - 27
+  mut.insert(4, 22); // 15 - 22
+  mut.insert(5, 5); // 29 - 5
+  mut.insert(6, 6); // 31 - 6
+  mut.insert(7, 13); // 33 - 13
+  mut.insert(8, 19); // 35 - 19
+
+  // TODO: add control logic.
+}
+
